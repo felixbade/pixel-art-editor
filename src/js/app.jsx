c3341b6b7a7fd811935261ac80fb6d094e8d874e
Felix Bade <flix.bade@gmail.com>
2021-04-12 00:25:58 +0300

    Add tiling mini view

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,6 +6,24 @@ const colors = ['',
     'hsl(140, 30%, 50%)',
     'hsl(140, 30%, 75%)']
 
+// eslint-disable-next-line react/prop-types
+const Drawing = ({ drawing }) => {
+    return (
+        <table cellSpacing="0" className="small">
+            <tbody>
+                {/* eslint-disable-next-line react/prop-types */}
+                {drawing.map((row, y) =>
+                    <tr key={y}>
+                        {row.map((cell, x) => 
+                            <td key={[x, y]} style={{ background: colors[cell] }}></td>
+                        )}
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
+
 export default function App() {
     const [color, setColor] = useState(2)
     const [drawing, setDrawing] = useState(Array(16).fill(Array(16).fill(0)))
@@ -27,7 +45,7 @@ export default function App() {
             <button onClick={() => setColor(2)} style={{ background: colors[2] }} className={color === 2 ? 'selected': ''}>Color 2</button>
             <button onClick={() => setColor(3)} style={{ background: colors[3] }} className={color === 3 ? 'selected': ''}>Color 3</button>
             <br/>
-            <table cellSpacing="0">
+            <table cellSpacing="0" className="drawing">
                 <tbody>
                     {drawing.map((row, y) =>
                         <tr key={y}>
@@ -39,6 +57,22 @@ export default function App() {
                 </tbody>
             </table>
             <input type="text" value={JSON.stringify(drawing)} onChange={(event) => setDrawing(JSON.parse(event.target.value))}></input>
+
+            <br/>
+            <br/>
+            <table cellSpacing="0">
+                <tbody>
+                    {Array(4).fill(null).map((_, i) => i).map((y) =>
+                        <tr key={y}>
+                            {Array(4).fill(null).map((_, i) => i).map((x) =>
+                                <td key={[x, y]}>
+                                    <Drawing drawing={drawing} />
+                                </td>
+                            )}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </>
     )
 }
