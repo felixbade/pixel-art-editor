80e25894d8d654bf132216c93f3c3bfbb179cca4
Felix Bade <flix.bade@gmail.com>
2021-04-11 23:06:31 +0300

    Add 16x16 drawing grid

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,8 +1,19 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 export default function App() {
     const [color, setColor] = useState(0)
+    const [drawing, setDrawing] = useState(Array(16).fill(Array(16).fill(0)))
+
+    const drawPixel = (x, y, c) => {
+        let drawing2 = Array(16)
+        drawing.map((row, y) => {
+            // Needed for React to re-render
+            drawing2[y] = [...row]
+        })
+        drawing2[y][x] = c
+        setDrawing(drawing2)
+    }
 
     return (
         <>
@@ -11,7 +22,18 @@ export default function App() {
             <button onClick={() => setColor(2)}>Color 2</button>
             <button onClick={() => setColor(3)}>Color 3</button>
             <br/>
-            <div>{color}</div>
+            <div>Drawing with color: {color}</div>
+            <table>
+                <tbody>
+                    {drawing.map((row, y) =>
+                        <tr key={y}>
+                            {row.map((cell, x) => 
+                                <td key={[x, y]} onClick={() => {drawPixel(x, y, color)}}>{cell}</td>
+                            )}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </>
     )
 }
