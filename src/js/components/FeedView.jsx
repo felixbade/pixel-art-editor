4e09ca5d28654c80f0a9bf5dd7dd776c1fecc124
Felix Bade <flix.bade@gmail.com>
2021-04-14 00:38:40 +0300

    Add FeedView

--- src/js/components/FeedView.jsx
+++ src/js/components/FeedView.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { getSprites } from '../api'
+import { Drawing } from './Drawing'
+
+
+const colors = ['',
+    'hsl(140, 30%, 25%)',
+    'hsl(140, 30%, 50%)',
+    'hsl(140, 30%, 75%)']
 
 export const FeedView = () => {
+
+    const [drawings, setDrawings] = useState([])
+
+    useEffect(() => {
+        getSprites().then((data) => {
+            setDrawings(data)
+        })
+    }, [])
+
     return (
-        <h1>FeedView</h1>
+        <>
+            <ul>
+                {drawings.map((drawing, i) =>
+                    <li key={i}>
+                        <Drawing drawing={drawing.graphic} colors={colors} />
+                    </li>
+                )}
+            </ul>
+        </>
     )
 }
